db1150c782237d775fdf7909bb7ece9fd10d4b90
seyfemichael <welcomesmtm@gmail.com>
2022-05-18 20:03:17 -0400

    initioal commit

--- main.c
+++ main.c
@@ -1,73 +1,51 @@
 #include "main.h"
 
 /**
- * free_data - frees data structure
- *
- * @datash: data structure
- * Return: no return
+ * main - Main function
+ * @argc: Number of arguments
+ * @argv: List of arguments
+ * @envp: List of enviroments
+ * Return: 0
  */
-void free_data(data_shell *datash)
+int main(int argc, char **argv, char *envp[])
 {
-	unsigned int i;
+	vars_t vars = {NULL, NULL};
+	size_t len_buffer = 0;
+	int i;
 
-	for (i = 0; datash->_environ[i]; i++)
+	(void)argc;
+	(void)argv;
+
+	_puts("$ ");
+
+	while ((getline(&(vars.buffer), &len_buffer, stdin)) != -1)
 	{
-		free(datash->_environ[i]);
+
+		if (!strcmp(vars.buffer, "env\n"))
+		{
+			for (i = 0 ; envp[i] ; i++)
+			{
+				_puts(envp[i]);
+				_putchar('\n');
+			}
+		}
+
+		vars.array_tokens = tokenizer(vars.buffer, " \n");
+		if (vars.array_tokens == NULL)
+		{
+			_puts("$ ");
+			continue;
+		}
+
+		if (check_for_builtins(&vars) == NULL)
+		{
+			_puts("No encontrado ");
+			_puts(vars.array_tokens[0]);
+			_putchar('\n');
+		}
+		free_shell(&vars);
+		_puts("$ ");
 	}
-
-	free(datash->_environ);
-	free(datash->pid);
-}
-
-/**
- * set_data - Initialize data structure
- *
- * @datash: data structure
- * @av: argument vector
- * Return: no return
- */
-void set_data(data_shell *datash, char **av)
-{
-	unsigned int i;
-
-	datash->av = av;
-	datash->input = NULL;
-	datash->args = NULL;
-	datash->status = 0;
-	datash->counter = 1;
-
-	for (i = 0; environ[i]; i++)
-		;
-
-	datash->_environ = malloc(sizeof(char *) * (i + 1));
-
-	for (i = 0; environ[i]; i++)
-	{
-		datash->_environ[i] = _strdup(environ[i]);
-	}
-
-	datash->_environ[i] = NULL;
-	datash->pid = aux_itoa(getpid());
-}
-
-/**
- * main - Entry point
- *
- * @ac: argument count
- * @av: argument vector
- *
- * Return: 0 on success.
- */
-int main(int ac, char **av)
-{
-	data_shell datash;
-	(void) ac;
-
-	signal(SIGINT, get_sigint);
-	set_data(&datash, av);
-	shell_loop(&datash);
-	free_data(&datash);
-	if (datash.status < 0)
-		return (255);
-	return (datash.status);
+	_putchar('\n');
+	exit(0);
 }
